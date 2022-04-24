03e07b3747b3606ec7149f31189de618949cd11c
andregugelmin <andregugelmin1996@gmail.com>
2022-04-24 12:37:48 -0300

    feat: data validation, status codes, tweet by username, tweets with pagination, user by header

--- index.js
+++ index.js
@@ -11,37 +11,71 @@ let tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
-    const user = {
-        username: username,
-        avatar: avatar,
-    };
 
-    users.push(user);
-    res.send('OK');
+    if (!username || !avatar) {
+        res.status(400).send('Todos os campos são obrigatórios!');
+    } else {
+        const checkUser = users.find((user) => user.username === username);
+        if (checkUser) {
+            res.status(400).send('User already exists');
+        } else {
+            users.push(req.body);
+            console.log(
+                chalk.bold.yellow(`User ${req.body.username} registered`)
+            );
+            res.status(201).send('OK');
+        }
+    }
 });
 
 app.post('/tweets', (req, res) => {
-    const { username, tweet } = req.body;
+    const tweet = req.body.tweet;
+    const username = req.headers.user;
+    console.log(username);
+    if (!username || !tweet) {
+        res.status(400).send('Todos os campos são obrigatórios!');
+        console.log(chalk.bold.yellow(`Error post tweets`));
+    } else {
+        let tweetUser = users.find((user) => user.username === username);
+        const tweetPost = {
+            username: username,
+            avatar: tweetUser.avatar,
+            tweet: tweet,
+        };
 
-    let tweetUser = users.find((user) => user.username === username);
-    const tweetPost = {
-        username: username,
-        avatar: tweetUser.avatar,
-        tweet: tweet,
-    };
-
-    tweets.unshift(tweetPost);
-
-    res.send('OK');
+        tweets.unshift(tweetPost);
+        console.log(chalk.bold.yellow(`tweet posted ${tweetPost}`));
+        res.status(201).send('OK');
+    }
 });
 
 app.get('/tweets', (req, res) => {
-    const totalTweets = tweets.length > 10 ? 10 : tweets.length;
-    let lastsTweets = [];
-    for (let i = 0; i < totalTweets; i++) {
-        lastsTweets.push(tweets[i]);
+    const page = parseInt(req.query.page);
+    if (!page || page < 1) {
+        res.status(400).send('Informe uma página válida!');
+        console.log(chalk.bold.yellow(`Error get tweets`));
+    } else {
+        let i = 10 * (page - 1);
+        const totalTweets = 10 * (page - 1) + 10;
+        let showtweets = [];
+        while (i < totalTweets && tweets[i] !== undefined) {
+            showtweets.push(tweets[i]);
+            i++;
+            console.log(chalk.bold.red(`${i} ${tweets[i]}`));
+        }
+        console.log(chalk.bold.yellow(`tweets geted ${showtweets}`));
+        res.send(showtweets);
     }
-    res.send(lastsTweets);
+});
+
+app.get('/tweets/:USERNAME', (req, res) => {
+    const USERNAME = req.params.USERNAME;
+
+    const userTweets = tweets.filter(
+        (currentTweet) => USERNAME === currentTweet.username
+    );
+
+    res.send(userTweets);
 });
 
 app.listen(5000);
